13f41cbabad5f3c4f690d7fef6e92a4ce7260988
abaicus <baicusandrei@me.com>
2016-02-11 03:09:47 +0200

    Fixin merge conflict

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -28,35 +28,28 @@
 	</header><!-- .entry-header -->
 
 	<div class="post-thumbnail-single">
-		<?php 
+		<?php
 			if ( has_post_thumbnail() ) {
 				the_post_thumbnail();
 			}
-		?> 
+		?>
 	</div>
 
 	<div class="entry-content">
 
-		<?php
-			
-			$pos = strpos( $post->post_content, '<!--more-->' );
+			<?php
 
-			if ( $pos <= 0 ) {
-				the_excerpt();
-			} else {
 				the_content( sprintf(
 					/* translators: %s: Name of current post. */
-					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'lawyeriax-lite' ), array( 'span' => array( 'class' => array() ) ) ),
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'qwertyuiop' ), array( 'span' => array( 'class' => array() ) ) ),
 					the_title( '<span class="screen-reader-text">"', '"</span>', false )
 				) );
-			}
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'lawyeriax-lite' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'qwertyuiop' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
 		<?php lawyeriax_lite_entry_footer(); ?>
