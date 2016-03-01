6000ce9718f5e037ba4ba8b673424df817f4b825
claudiuvertistudio <claudiu.buruiana@codeinwp.com>
2016-03-01 14:36:29 +0200

    Fixed 10 Added styling for feature box

--- inc/frontpage-sections.php
+++ inc/frontpage-sections.php
@@ -142,62 +142,74 @@ endif;
   * Features sections
   */
  function lawyeriax_lite_features_section() {
+
+      $feature_box_first = get_theme_mod('first_feature_box');
+      $feature_box_second = get_theme_mod('second_feature_box');
+      $feature_box_third = get_theme_mod('third_feature_box');
+      $feature_box_fourth = get_theme_mod('fourth_feature_box');
+
+      $box_ids = array( $feature_box_first, $feature_box_second, $feature_box_third, $feature_box_fourth );
+
+      if ( array_sum($box_ids) > 0 ) {
+
   ?>
 
- 	<section id="features" class="features">
- 		<div class="container">
- 			<div class="home-section-inner feature-boxes-wrap">
-
-        <?php
-        $box_ids = array(get_theme_mod('first_feature_box'), get_theme_mod('second_feature_box'), get_theme_mod('third_feature_box'), get_theme_mod('fourth_feature_box'));
-        if(!empty($box_ids)) {
-          foreach($box_ids as $id) {
-            if($id != 0) {
-            $page = get_post($id); ?>
-            <div class="col-xs-12 col-sm-6 col-md-3 feature-box">
-              <div class="features-title">
-                <span class="features-title-icon"><i class="fa fa-gavel"></i></span>
-
-                <?php if(!empty($page->post_title)) { ?>
-
-                  <div class="feature-title-wrap">
-                    <h3 class="feature-title"><?php echo esc_html($page->post_title); ?></h3>
-                  </div>
-
-                <?php } ?>
-
-              </div>
-
-              <?php
-
-               if(!empty($page->post_excerpt)) { ?>
-                <div class="border-left features-content">
-                  <p><?php echo esc_html(substr($page->post_excerpt, 0, 100)) . '...'; ?></p>
-                  <a href="<?php echo the_permalink($id); ?>" title="Read more" class="read-more"> <?php echo __('Read more...', 'lawyeriax-lite') ?> </a>
-                </div>
-
-              <?php } else if(!empty($page->post_content)) { ?>
-
-                <div class="border-left features-content">
-                  <p><?php echo esc_html(substr($page->post_content, 0 , 100)) . '...'; ?></p>
-                  <a href="<?php echo the_permalink($id); ?>" title="Read more" class="read-more"> <?php echo __('Read more...', 'lawyeriax-lite') ?> </a>
-                </div>
-
-              <?php } ?>
-
-            </div>
+     	<section id="features" class="features">
+     		<div class="container">
+     			<div class="home-section-inner feature-boxes-wrap">
 
             <?php
+
+            if(!empty($box_ids)) {
+              foreach($box_ids as $id) {
+                if($id != 0) {
+                $page = get_post($id); ?>
+                <div class="col-xs-12 col-sm-6 col-md-3 feature-box">
+                  <div class="features-title">
+                    <span class="features-title-icon"><i class="fa fa-gavel"></i></span>
+
+                    <?php if(!empty($page->post_title)) { ?>
+
+                      <div class="feature-title-wrap">
+                        <h3 class="feature-title"><?php echo esc_html($page->post_title); ?></h3>
+                      </div>
+
+                    <?php } ?>
+
+                  </div>
+
+                  <?php
+
+                   if(!empty($page->post_excerpt)) { ?>
+                    <div class="border-left features-content">
+                      <p><?php echo esc_html(substr($page->post_excerpt, 0, 100)) . '...'; ?></p>
+                      <a href="<?php echo the_permalink($id); ?>" title="Read more" class="read-more"> <?php echo __('Read more...', 'lawyeriax-lite') ?> </a>
+                    </div>
+
+                  <?php } else if(!empty($page->post_content)) { ?>
+
+                    <div class="border-left features-content">
+                      <p><?php echo esc_html(substr($page->post_content, 0 , 100)) . '...'; ?></p>
+                      <a href="<?php echo the_permalink($id); ?>" title="Read more" class="read-more"> <?php echo __('Read more...', 'lawyeriax-lite') ?> </a>
+                    </div>
+
+                  <?php } ?>
+
+                </div>
+
+                <?php
+              }
+            }
           }
-        }
-      }
         ?>
 
       </div>
     <div class="col-sm-10 col-sm-offset-1 section-line"></div>
   </div><!-- .container -->
 </section>
- <?php }
+ 
+<?php } 
+  }
 endif;
 
  if ( ! function_exists( 'lawyeriax_lite_about_us_section' ) ) :
